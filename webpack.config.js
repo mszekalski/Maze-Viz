64e7f927896d50f54745135d8d9c056ebd88fbf8
Matthew Szekalski <mattszekalski@icloud.com>
2019-01-02 15:55:47 -0500

    fixed bundle issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require("path");
 
 module.exports = {
-    context: __dirname,
-    entry: "./lib/maze.js",
-    output: {
-        path: path.resolve(__dirname, "lib"),
-        filename: "bundle.js"
-    },
-    devtool: "source-map"
+  context: __dirname,
+  entry: "./lib/maze.js",
+  output: {
+    path: path.resolve(__dirname, "lib"),
+    filename: "bundle.js"
+  },
+  devtool: "source-map"
 };
